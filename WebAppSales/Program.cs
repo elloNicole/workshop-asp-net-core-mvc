7d9071b54c48ccb97cf5e08ceee471c2b5c44491
Nicole Mello <nicolemello6002@gmail.com>
2024-06-23 22:12:38 -0300

    Primeira migration

--- WebAppSales/Program.cs
+++ WebAppSales/Program.cs
@@ -1,9 +1,33 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
 using Microsoft.Extensions.DependencyInjection;
+using Pomelo.EntityFrameworkCore.MySql;
 using WebAppSales.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Verifique se a connection string não é nula
+var connectionString = builder.Configuration.GetConnectionString("WebAppSalesContext");
+if (connectionString == null)
+{
+    throw new InvalidOperationException("Connection string 'WebAppSalesContext' not found.");
+}
+
+// Configuração do DbContext com resiliência a erros transitórios
 builder.Services.AddDbContext<WebAppSalesContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("WebAppSalesContext") ?? throw new InvalidOperationException("Connection string 'WebAppSalesContext' not found.")));
+    options.UseMySql(
+        builder.Configuration.GetConnectionString("WebAppSalesContext"),
+        new MySqlServerVersion(new Version(8, 0, 23)),
+        mySqlOptions =>
+        {
+            mySqlOptions.EnableRetryOnFailure(
+                maxRetryCount: 5, // Número máximo de tentativas
+                maxRetryDelay: TimeSpan.FromSeconds(10), // Tempo máximo de espera entre as tentativas
+                errorNumbersToAdd: null // Lista de números de erro adicionais que desencadeiam uma nova tentativa
+            );
+        }
+    )
+);
 
 // Adicione os servi�os necess�rios ao cont�iner.
 builder.Services.AddControllersWithViews();
